7b5e0813d8d17534222ce1cfb21d3afc4064364b
Gotam Gorabh <gautamy672@gmail.com>
2022-10-25 22:48:42 +0530

    fetching headlines from api

--- clinews/src/main.rs
+++ clinews/src/main.rs
@@ -1,14 +1,32 @@
+use std::error::Error;
 
-fn get_articles(url: &str){
+struct Articles{
+   
+   articles: Vec<Article>  
+}
+
+
+struct Article{
     
-    let responce = ureq::get(url).call()?.into_string()?;
+}
+
+
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>>{
+    
+    let response = ureq::get(url).call()?.into_string()?;
+    
+    dbg!(response);
+    
+    todo!()
     
 }
 
 
 fn main() {
 
-    let url: &str = "https://newsapi.org/v2/top-headlines?country=in&apiKey=API_KEY";
-    let articles = get_articles(url);
+    // contains top headlines end point as a string
+    let url: &str = "https://newsapi.org/v2/top-headlines?country=in&apiKey=81ad1c2efc7049d8882727ee26749dc1";
+    
+    let articles: Result<Articles, Box<dyn Error>> = get_articles(url);
     
 }
