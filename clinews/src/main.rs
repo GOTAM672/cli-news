9ceffd3e0f9dda4aed7db95d124c632df828b2b5
Gotam Gorabh <gautamy672@gmail.com>
2022-10-25 20:59:56 +0530

    adding api & dependency

--- clinews/src/main.rs
+++ clinews/src/main.rs
@@ -1,3 +1,14 @@
+
+fn get_articles(url: &str){
+    
+    let responce = ureq::get(url).call()?.into_string()?;
+    
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let url: &str = "https://newsapi.org/v2/top-headlines?country=in&apiKey=API_KEY";
+    let articles = get_articles(url);
+    
 }
